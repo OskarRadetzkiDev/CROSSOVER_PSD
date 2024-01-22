594a5cce2e8695aaab32ed88c438e923cf22932f
Oskar <oskar.radetzki.dev@googlemail.com>
2024-01-22 15:44:23 +0100

    Section 8 & 7

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import NavbarHero from './components/Nav/NavbarHero'
 import Cards from './components/Cards/Cards'
 import Info from './components/Info/Info'
+import Component8 from './components/Component8'
+import Component7 from './components/Component7'
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
     <NavbarHero />
     <Cards />
     <Info />
+    <Component7 />
+    <Component8 />
     <Footer />
     </>
   )
---
+++ src/components/Component8.jsx
@@ -0,0 +1,15 @@
+import '../Component8.css'
+import './chr.css'
+
+function component8() {
+  return (
+    <div class="component8-bg">
+        <p class="c8-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum.</p>
+        <div>
+            <h2 class="c8-h2">Dr. jenny</h2>
+        </div>
+    </div>
+  )
+}
+
+export default component8
