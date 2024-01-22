7ab7684fb0ebd6e44ac267e1a5854f341b6d20b1
DarkoHristovski <hristovski15@gmail.com>
2024-01-22 15:14:03 +0100

    done darko sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css'
+import "./App.css";
+import Component4 from "./components/component4";
+import Component5 from "./components/component5";
+import Component6 from "./components/component6";
 
 function App() {
-
   return (
     <>
+      <Component4 />
+      <Component5 />
+      <Component6 />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
